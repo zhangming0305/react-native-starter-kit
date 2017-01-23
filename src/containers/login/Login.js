666b492d2352c0c05996819ddc033d5cfed0f797
lixiaoyang <lixiaoyang2345@gmail.com>
2017-01-23 17:20:57 +0800

    Fix

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -1,12 +1,9 @@
-/* tslint:disable:no-unused-variable */
 import React from 'react';
-/* tslint:enable:no-unused-variable */
-import { View, Text, Alert } from 'react-native';
-import { Button, WhiteSpace, WingBlank, List, InputItem } from 'antd-mobile';
+import { View } from 'react-native';
+import { Button, WhiteSpace, WingBlank, InputItem } from 'antd-mobile';
 import { createForm } from 'rc-form';
 import { Actions } from 'react-native-router-flux';
 
-/* tslint:disable:no-console */
 const Login = ({ form: { getFieldProps } }) => (
   <View>
     <InputItem
@@ -23,9 +20,7 @@ const Login = ({ form: { getFieldProps } }) => (
     >
       密码
     </InputItem>
-
     <WhiteSpace />
-
     <WingBlank>
       <Button
         type="primary"
@@ -40,5 +35,9 @@ const Login = ({ form: { getFieldProps } }) => (
     </WingBlank>
   </View>
 );
+
+Login.propTypes = {
+  form: React.PropTypes.object.isRequired,
+};
 
 export default createForm()(Login);
